d4129d567011a6888b3edf7a22ce125a61533d61
Sharkzinho <98825649+Shark1Galaxy@users.noreply.github.com>
2025-08-11 12:21:04 -0300

    Aprendendo sobre vaviáveis.
    Aqui to aprendendo sobre, o que são, como usá-las e o que cada um é:
    Const,var e let;

--- Exercicios/var.js
+++ Exercicios/var.js
@@ -1,1 +1,19 @@
-let;
+let idade = 23;
+console.log(idade);
+let altura = 1.7;
+console.log(altura);
+//Melhor forma de criar variáveis e rfecomenda, e usando camelCase. Exemplo abaixo (Boas práticas de programação) mais cuidado, JavaScript distingue letras maiúsculas de minúsculas em nomes de variáveis, ou seja, elas são sensíveis a maiúsculas e minúsculas. e ficam como variaveis diferentes.
+let nomeCompleto = "João da Silva";
+console.log(nomeCompleto);
+
+//Constantes
+let valorIngressoAdulto = 20;
+valorIngressoAdulto = 30;
+console.log(valorIngressoAdulto);
+
+//Mais usando o Const, ele não permite que caso eu tente mudar o valro da varivel, ele não deixa, ou seja, o valor é fixo.
+const valorIngressoIdoso = 20;
+valorIngressoIdoso = 30; // Isso vai gerar um erro, pois não é possível reatribuir um valor a uma constante.
+console.log(valorIngressoIdoso); // Isso não será executado devido ao erro anterior.
+
+/*Ou seja, so use o let se vc quer mudar os valores da varivels, e o const caso queira que o valor seja fixo */
